5d0218044089c046d5030f9aad963a3c32a44494
MimiDumpling <tienmiminguyen@gmail.com>
2018-06-05 17:05:56 -0700

    playing around with frontend api

--- config/api.js
+++ config/api.js
@@ -1,14 +1,81 @@
-function createUser( userName ) {
-  console.log(userName);
+// createUserInDB(json) { ...do things... }
+import { createUserInDB } from 'config/db';
+// createBoardFromUserInDB(user) { ...do things... }
+import { createBoardFromUserInDB } from 'config/db';
+import { react } from 'babel-types';
+
+function createUser( response ) {
+  const json = response.json();
+  return createUserInDB(json)
 };
 
-function createBoard( boardTitle ) {
-  console.log(boardTitle);
+function createBoard( user ) {
+  createBoardFromUserInDB(user);
 };
 
-function createPost( postTitle ) {
-  console.log(postTitle)
+fetch({url: 'https://jsonplaceholder.typicode.com/posts/1'})
+  .then(createUser)
+  .then(createBoard)
+
+class Post extends React.Component {
+  handlePress() {
+    // fetch the post
+    fetch('www.pinterestclone.com/post')
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          post: json,
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.post}
+        <button onPress={this.handlePress}>Get post</button>
+      </div>
+    );
+  }
+}
+
+// one json method: sets State
+function changeState(json) {
+  this.setState({
+    post: json,
+  })
 };
 
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(createUser(userName))
+// json method: console.log the json
+function logJson(json) {
+  console.log(json)
+};
+
+// base method
+function base( path, jsonMethod ) {
+  fetch('www.pinterestclone.com' + path)
+    .then(response => response.json())
+    .then(jsonMethod)
+    })
+};
+
+function getPost(id) {
+  return base('/post/' + id, logJson)
+};
+
+function getPosts() {
+  return base('/posts', logJson)
+};
+
+function getComment(num) {
+  //not sure if this is supported by jsonplaceholder
+  return base('/comment/' + id, logJson)
+};
+
+function getComments() {
+  return base('/comments', logJson)
+};
+
+function getCommentsForPost(id) {
+  return base('	/comments?postId=' + id, logJson)
+};
